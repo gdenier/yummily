5df6374cddbaa286b23eb8fba9957bb47bb925f5
Guillaume <guillaume_denier@hotmail.fr>
2023-04-19 19:43:55 +0200

    fix build v2

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -1,17 +1,14 @@
-// import { AddButton } from "./components/AddButton"
-// import { fetcher } from "~/lib/api/utils/fetcher"
-// import { SignOutButton } from "~/components/auth/SignOutButton"
+import { AddButton } from "./components/AddButton"
+import { SignOutButton } from "~/components/auth/SignOutButton"
 
-// export default async function HelloPage() {
-//   const hellos = await fetcher("hello")
-
-//   return (
-//     <div className="flex flex-col">
-//       {hellos?.map((hello: any) => (
-//         <p key={hello.id}>{hello.title}</p>
-//       ))}
-//       <AddButton />
-//       <SignOutButton />
-//     </div>
-//   )
-// }
+export default async function HelloPage() {
+  return (
+    <div className="flex flex-col">
+      {[]?.map((hello: any) => (
+        <p key={hello.id}>{hello.title}</p>
+      ))}
+      <AddButton />
+      <SignOutButton />
+    </div>
+  )
+}
