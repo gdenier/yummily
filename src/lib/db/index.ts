49392d5804ee7f9ea8e7b979322103f6187f59fb
Guillaume <guillaume_denier@hotmail.fr>
2023-04-20 12:23:57 +0200

    remove log

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,7 +2,6 @@ import { Pool } from "pg"
 import { drizzle } from "drizzle-orm/node-postgres"
 import { env } from "./env"
 
-console.log(env.DATABASE_URL)
 const pool = new Pool({
   connectionString: env.DATABASE_URL,
 })
